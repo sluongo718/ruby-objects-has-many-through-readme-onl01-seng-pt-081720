8c9efc426c9198b121affcecf053758710ee0f07
sam l <sluongo718@yahoo.com>
2020-09-16 15:56:33 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -4,4 +4,27 @@ class Customer
   
   @@all = []
   
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 end
