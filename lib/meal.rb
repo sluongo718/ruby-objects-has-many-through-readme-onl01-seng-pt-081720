0487ad872c43406f1619ee1d9797d4d5bbdd255c
sam l <sluongo718@yahoo.com>
2020-09-16 16:07:14 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,37 @@
 class Meal
+  
+  attr_accessor :waiter, :customer, :total, :tip 
+  
+  @@all 
+  
+  def initialize(waiters, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
 
 end
